469eaf12ed92f3f846c69c32d479376369c9b907
Chris Houhoulis <chouhoulis@comcast.net>
2010-09-08 10:43:08 -0400

    eliminated superfluous controller methods/views.

--- testrailsapp/app/controllers/tommy_boy_controller.rb
+++ testrailsapp/app/controllers/tommy_boy_controller.rb
@@ -7,23 +7,12 @@ class TommyBoyController < ApplicationController
   end
 
   def ebay
-#    render :text => proc{|resp,output| 1.times {|i| output.write("ebay." + "html")}}, :content_type => "text/plain"
-    render :text => 'This Is The Controller.  not ebay_html.' # , :content_type => 'text/plain'
-  end
-
-  def ebay2
-#    render :text => proc{|resp,output| 1.times {|i| output.write("ebay." + "html")}}, :content_type => "text/plain"
-#    render :text => 'ebay.html' # , :content_type => 'text/plain'
-    render :text => "I biting FROM CONTROLLER e-bay-2-html", :content_type => 'text/plain'
+    render :text => 'This is the controller -- not ebay_html static file.',
+      :content_type => 'text/plain'
   end
 
   def mattknox
     render :text => "http://mattknox.com", :content_type => "text/plain"
-  end
-
-  def mcr
-    render :text => "http://masscomputerrelief.com",
-      :content_type => "text/plain"
   end
 
   def env
