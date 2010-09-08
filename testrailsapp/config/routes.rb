dd92e8c30a373484a0d23e94baca217d29a0d8cb
Chris Houhoulis <chouhoulis@comcast.net>
2010-09-08 10:43:08 -0400

    eliminated superfluous controller methods/views.

--- testrailsapp/config/routes.rb
+++ testrailsapp/config/routes.rb
@@ -2,7 +2,5 @@ ActionController::Routing::Routes.draw do |map|
   map.root :controller => "tommy_boy", :action => "index"
   map.connect 'more', :controller => "tommy_boy", :action => "more"
   map.connect 'ebay', :controller => "tommy_boy", :action => "ebay"
-  map.connect 'ebay2', :controller => "tommy_boy", :action => "ebay2"
-  map.connect 'mcr', :controller => "tommy_boy", :action => "mcr"
   map.connect 'mattknox', :controller => "tommy_boy", :action => "mattknox"
 end
